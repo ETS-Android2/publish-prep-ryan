da0775f0915718b255b5e358925f6b90047cdcde
RyanShivaXorbix <ryan.shiva@xorbix.com>
2021-10-28 17:06:15 -0500

    constants updated to x

--- lib/document_view.dart
+++ lib/document_view.dart
@@ -249,4 +249,24 @@ class DocumentViewController {
   Future<void> openAnnotationList() {
     return _channel.invokeMethod(Functions.openAnnotationList);
   }
+
+/*  /// Determines visibility of toolbar
+  /// Xorbix function
+  Future<void> markupOptionSelected(bool markupSelected) {
+    return _channel.invokeMethod(Functions.markupOptionSelected,
+        <String, dynamic>{Parameters.markupSelected: markupSelected});
+  }
+
+  /// Create a document using a subset of pages from the original document
+  /// Xorbix function
+  /// Accepts the filepath for the original document, the start and end pages, and the annotations to import
+  Future<dynamic> createDocFromPageRangeWithAnnotations(String sourceDocPath, int startPage, int endPage, String annotations) {
+    return _channel.invokeMethod(Functions.createDocFromPageRangeWithAnnotations,
+        <String, dynamic>{
+          Parameters.sourceDocPath: sourceDocPath,
+          Parameters.startPage: startPage,
+          Parameters.endPage: endPage,
+          Parameters.xorbixAnnotations: annotations
+        });
+  }*/
 }
