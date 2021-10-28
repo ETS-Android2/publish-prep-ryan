641ee308fe23fcdaf926c4bd24679cd6c2cb78dd
RyanShivaXorbix <ryan.shiva@xorbix.com>
2021-10-28 13:20:48 -0500

    lib changed to publish-prep-x

--- lib/constants.dart
+++ lib/constants.dart
@@ -31,16 +31,10 @@ class Functions {
   static const deleteAllAnnotations = "deleteAllAnnotations";
   static const openAnnotationList = "openAnnotationList";
   static const setRequestedOrientation = "setRequestedOrientation";
-  static const gotoPreviousPage = "gotoPreviousPage";
-  static const gotoNextPage = "gotoNextPage";
-  static const gotoFirstPage = "gotoFirstPage";
-  static const gotoLastPage = "gotoLastPage";
-  static const addBookmark = "addBookmark";
-  static const openBookmarkList = "openBookmarkList";
-  static const openOutlineList = "openOutlineList";
-  static const openLayersList = "openLayersList";
-  static const openNavigationLists = "openNavigationLists";
-  static const getCurrentPage = "getCurrentPage";
+
+  // Xorbix functions
+  static const markupOptionSelected = "markupOptionSelected";
+  static const createDocFromPageRangeWithAnnotations = "createDocFromPageRangeWithAnnotations";
 }
 
 /// Defines the parameters of the functions.
@@ -53,7 +47,6 @@ class Parameters {
   static const xfdf = "xfdf";
   static const bookmarkJson = "bookmarkJson";
   static const pageNumber = "pageNumber";
-  static const title = "title";
   static const toolMode = "toolMode";
   static const fieldNames = "fieldNames";
   static const fields = "fields";
@@ -66,6 +59,13 @@ class Parameters {
   static const annotationProperties = "annotationProperties";
   static const leadingNavButtonIcon = "leadingNavButtonIcon";
   static const requestedOrientation = "requestedOrientation";
+
+  // Xorbix Parameters
+  static const markupSelected = "markupSelected";
+  static const sourceDocPath = "sourceDocPath";
+  static const startPage = "startPage";
+  static const endPage = "endPage";
+  static const xorbixAnnotations = "annotations";
 }
 
 /// Defines the parameters of the events.
